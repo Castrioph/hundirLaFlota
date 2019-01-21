8a449eeb69a41d94f8f4cb15ae480e8b2f801be2
marcosperezper <mapepe01@floridauniversitaria.es>
2019-01-21 19:34:24 +0100

    boatFunctions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,52 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'hundirLaFlota';
+    title = 'hundirLaFlota';
 
-  // CONSTANTES PARA LA ORIENTACIÓN DE LOS BARCOS
-  ORIENTATION_TO_UP_AND_DOWN : number = 0;
-  ORIENTATION_TO_LEFT_AND_RIGHT : number = 1;
+    // CONSTANTES PARA LA ORIENTACIÓN DE LOS BARCOS
+    ORIENTATION_TO_UP_AND_DOWN: number = 0;
+    ORIENTATION_TO_LEFT_AND_RIGHT: number = 1;
 
-  //CONSTANTES PARA LA DIRECCIÓN DE LOS BARCOS ORIENTADOS VERTICALMENTE
-  DIRECTION_TO_UP : number = 0;
-  DIRECTION_TO_DOWN : number = 1;
+    //CONSTANTES PARA LA DIRECCIÓN DE LOS BARCOS ORIENTADOS VERTICALMENTE
+    DIRECTION_TO_UP: number = 0;
+    DIRECTION_TO_DOWN: number = 1;
 
-  //CONSTANTES PARA LA DIRECCIÓN DE LOS BARCOS HORIENTADOS HORIZONTALMENTE
-  DIRECTION_TO_RIGHT : number = 0;
-  DIRECTION_TO_LEFT : number = 1;
+    //CONSTANTES PARA LA DIRECCIÓN DE LOS BARCOS HORIENTADOS HORIZONTALMENTE
+    DIRECTION_TO_RIGHT: number = 0;
+    DIRECTION_TO_LEFT: number = 1;
+
+    //Function to get boat orientation
+    boatOrientation() {
+        let orientation = 0;
+        let random = Math.round(Math.random());
+        if (random === 0) {
+            orientation = this.ORIENTATION_TO_LEFT_AND_RIGHT;
+        } else {
+            orientation = this.ORIENTATION_TO_UP_AND_DOWN;
+        }
+        return orientation;
+    }
+
+//Funciton to get boat position
+    boatPosition() {
+        let position = Math.round(Math.random() * 24);
+        let cell = document.getElementById('piece' + position);
+        return cell;
+    }
+
+//Function to get boat direction
+    boatDirection(orientation) {
+        let direction =0;
+        if (orientation===this.ORIENTATION_TO_UP_AND_DOWN) {
+            direction
+        }
+    }
 }
 
 
