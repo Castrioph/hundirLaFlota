84675feb9348aa55e192f5c626952d6424d5d59e
marcosperezper <mapepe01@floridauniversitaria.es>
2019-01-26 17:12:13 +0100

    functions_Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {gameConstants} from './constants/gameConstants';
+import {doesNotThrow} from "assert";
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
@@ -15,25 +16,24 @@ export class AppComponent {
     buttonText: string = 'Iniciar juego';
 
 
-
-  // Function to get boat orientation
-  boatOrientation() {
-    let orientation;
-    let random = Math.round(Math.random());
-    if (random === 0) {
-      orientation = gameConstants.ORIENTATION_TO_UP_AND_DOWN;
-    } else {
-      orientation = gameConstants.ORIENTATION_TO_LEFT_AND_RIGHT;
+    // Function to get boat orientation
+    boatOrientation() {
+        let orientation;
+        let random = Math.round(Math.random());
+        if (random === 0) {
+            orientation = gameConstants.ORIENTATION_TO_UP_AND_DOWN;
+        } else {
+            orientation = gameConstants.ORIENTATION_TO_LEFT_AND_RIGHT;
+        }
+        return orientation;
     }
-    return orientation;
-  }
 
-  // Funciton to get boat position
-  boatPosition() {
-    let position = Math.round(Math.random() * 24);
-    let cell = document.getElementById(position);
-    return cell;
-  }
+    // Funciton to get boat position
+    boatPosition() {
+        let position = Math.round(Math.random() * 24);
+        let cell = document.getElementById('cell ' + position);
+        return cell;
+    }
 
 
     // Function to get boat direction
@@ -56,41 +56,57 @@ export class AppComponent {
         }
         return direction;
     }
-  
 
-  //Function to check movement
-  checkMovement(cell, direction, nPieces, arrayBarquitos) {
-    if (direction === gameConstants.DIRECTION_TO_UP) {
-      for (let i = nPieces; i <= 0; i--) {
-        if ((cell - 5) <0) {
-          return false;
-        } else if (arrayBarquitos.include(cell)) {
-            return false;
-          } else {
-              cell = cell - 5;
-            }
-              }
-    } else if (direction === gameConstants.DIRECTION_TO_DOWN) {
-                  for (let i = nPieces; i <= 0; i--) {
-                    if ((cell + 5) > 24) {
-                  return false;
+
+    //Function to check movement
+    checkMovement(cell, direction, nPieces, arrayBarquitos) {
+        let row = 0;
+        if (direction === gameConstants.DIRECTION_TO_UP) {
+            for (let i = nPieces; i <= 0; i--) {
+                if ((cell - 5) < 0) {
+                    return false;
                 } else if (arrayBarquitos.include(cell)) {
-                  return false;
+                    return false;
+                } else {
+                    cell = cell - 5;
+                }
+            }
+            return true;
+        } else if (direction === gameConstants.DIRECTION_TO_DOWN) {
+            for (let i = nPieces; i <= 0; i--) {
+                if ((cell + 5) > 24) {
+                    return false;
+                } else if (arrayBarquitos.include(cell)) {
+                    return false;
 
 
                 } else {
-                  cell = cell + 5;
+                    cell = cell + 5;
                 }
-      }
-     } else if (direction === gameConstants.DIRECTION_TO_RIGHT) {
-          if (cell + 2 > 24) {
-            return false;
-          } else {
+            }
             return true;
-          }
+        } else if (direction === gameConstants.DIRECTION_TO_RIGHT) {
+            row = cell / 5;
+            if (cell + (nPieces - 1) / 5 > row) {
+                return false;
+            } else if (arrayBarquitos.include(cell)) {
+                return false;
+
+            } else {
+                return true;
+            }
+        } else if (direction === gameConstants.DIRECTION_TO_LEFT) {
+            row = cell / 5;
+            if (cell - (nPieces - 1) / 5 > row) {
+                return false;
+            }
+        } else {
+            return true;
         }
-      }
     }
+}
+
+
        
 
 
